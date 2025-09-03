933deb7b331bb8eb98c62b3e18f309ed3f464266
Juan Bouzas <jb.gandara92@gmail.com>
2025-09-03 17:26:31 +0200

    Create global scss variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
+import "./styles/application.scss";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
